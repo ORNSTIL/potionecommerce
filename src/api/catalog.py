dba7d4b8828b9d196a43815245c8e72a9831e249
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-04-08 13:03:43 -0700

    Update catalog.py

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -14,7 +14,7 @@ def get_catalog():
             "sku": "GREEN_POTION_0",
             "name": "green potion",
             "quantity": num_green_potions,
-            "price": 10, 
+            "price": 40, 
             "potion_type": [0, 100, 0, 0],  # 100% green potion
         }
     ]
