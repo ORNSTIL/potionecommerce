8b716f52cf4cfdb9b7428d6865c19e5543aedb44
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-04-08 18:29:50 -0700

    Update bottler.py

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -20,12 +20,11 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
     for potion in potions_delivered:
         if potion.potion_type == [0, 100, 0, 0]:  # green potions
             with db.engine.begin() as connection:
-                # Correctly passing 'qty' as part of a dictionary
                 connection.execute(sqlalchemy.text("""
                     UPDATE global_inventory 
                     SET num_green_potions = num_green_potions + :qty 
                     WHERE id = 1
-                """), {'qty': potion.quantity})  # Note the change here
+                """), {'qty': potion.quantity})  
     return "OK"
 
 
@@ -34,7 +33,7 @@ def get_bottle_plan():
     with db.engine.begin() as connection:
         num_green_ml = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory WHERE id = 1")).scalar()
         potions_possible = num_green_ml // 100 # Each potion requires 100 ml
-        # No database modifications here, just planning
+
         return [{
             "potion_type": [0, 100, 0, 0],
             "quantity": potions_possible,
