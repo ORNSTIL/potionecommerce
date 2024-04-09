1b990d57b6e66285e74f9566a1aa3a76c475b041
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-04-08 18:23:19 -0700

    Update bottler.py

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -33,7 +33,7 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
 def get_bottle_plan():
     with db.engine.begin() as connection:
         num_green_ml = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory WHERE id = 1")).scalar()
-        potions_possible = num_green_ml // 100 + 1 # Each potion requires 100 ml
+        potions_possible = num_green_ml // 100 # Each potion requires 100 ml
         # No database modifications here, just planning
         return [{
             "potion_type": [0, 100, 0, 0],
