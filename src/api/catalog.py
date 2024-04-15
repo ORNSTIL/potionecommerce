b1802fe7bd1c72c3ed00aa687bb7a73af4cb40a3
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-04-14 18:12:23 -0700

    Update catalog.py

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -9,14 +9,30 @@ router = APIRouter()
 def get_catalog():
     with db.engine.begin() as connection:
         num_green_potions = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar()
-    if num_green_potions > 1:
+        num_red_potions = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory")).scalar()
+        num_blue_potions = connection.execute(sqlalchemy.text("SELECT num_blue_potions FROM global_inventory")).scalar()
+    if (num_green_potions > 1) or (num_red_potions > 1) or (num_blue_potions > 1):
         return [
             {
                 "sku": "GREEN_POTION_0",
                 "name": "green potion",
-                "quantity": 1,
+                "quantity": num_green_potions,
                 "price": 40, 
-                "potion_type": [0, 100, 0, 0],  # 100% green potion
+                "potion_type": [0, 100, 0, 0], 
+            },
+            {
+                "sku": "RED_POTION_0",
+                "name": "red potion",
+                "quantity": num_red_potions,
+                "price": 40, 
+                "potion_type": [100, 0, 0, 0], 
+            },
+            {
+                "sku": "BLUE_POTION_0",
+                "name": "blue potion",
+                "quantity": num_blue_potions,
+                "price": 40, 
+                "potion_type": [0, 0, 100, 0], 
             }
         ]
     else:
