765c27638c307e1e830ed29b6603dea000aa2a46
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-04-14 18:25:24 -0700

    Update admin.py

--- src/api/admin.py
+++ src/api/admin.py
@@ -16,5 +16,16 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
+     with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("UPDATE global_inventory SET 
+        num_green_ml = :reset_ml, 
+        num_red_ml = :reset_ml, 
+        num_blue_ml = :reset_ml, 
+        num_green_potions = :reset_potions, 
+        num_red_potions = :reset_potions, 
+        num_blue_potions = :reset_potions, 
+        gold = :reset_gold"),
+        {"reset_ml": 0, "reset_potions": 0, "reset_gold": 100})
+        
     return "OK"
 
