9fd15a3b1e4957c8a6a919f8a823f04e53ef3677
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-04-14 18:03:14 -0700

    Update bottler.py

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -22,16 +22,31 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
     with db.engine.begin() as connection:
         num_green = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar_one()
         green_ml = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).scalar_one()
+        num_red = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory")).scalar_one()
+        red_ml = connection.execute(sqlalchemy.text("SELECT num_red_ml FROM global_inventory")).scalar_one()
+        num_blue = connection.execute(sqlalchemy.text("SELECT num_blue_potions FROM global_inventory")).scalar_one()
+        blue_ml = connection.execute(sqlalchemy.text("SELECT num_blue_ml FROM global_inventory")).scalar_one()
         
         for potion in potions_delivered:
-            if potion.potion_type == [0, 100, 0, 0] or barrel.potion_type == [0, 100, 0, 0]:
+            if potion.potion_type == [0, 100, 0, 0]:
                 num_green += potion.quantity
                 green_ml -= potion.quantity*100
-                break
+            elif potion.potion_type == [100, 0, 0, 0]:
+                num_red += potion.quantity
+                red_ml -= potion.quantity*100
+            elif potion.potion_type == [0, 0, 100, 0]:
+                num_blue += potion.quantity
+                blue_ml -= potion.quantity*100
+
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = :green_potions"),{"green_potions": num_green})
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = :green_ml_count"),{"green_ml_count": green_ml})
+
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = :red_potions"),{"red_potions": num_red})
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = :red_ml_count"),{"red_ml_count": red_ml})
+
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_potions = :blue_potions"),{"blue_potions": blue_green})
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_ml = :blue_ml_count"),{"bue_ml_count": blue_ml})
         
-        connection.commit()
 
     return "OK"
 
