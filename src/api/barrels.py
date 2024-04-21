37c4246a59f7d6a44644b467e54ed36997a91a1b
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-04-21 08:31:54 -0700

    Update barrels.py

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -74,7 +74,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
             for barrel in wholesale_catalog:
                 if barrel.ml_per_barrel > available_ml or barrel.price > running_total:
                     continue
-                if potion_type["barrel_type"] == barrel.potion_type:
+                if potion_type["barrel_type"] == potion_type_tostr(barrel.potion_type):
                     barrel_plan.append({"sku": barrel.sku, "quantity": 1})
                     running_total -= barrel.price
                     available_ml -= barrel.ml_per_barrel
